aaf26bd9cb3bf4c7dd8fa6845927ab5531fdb1ae
Shashanks7s7 <52647286+Shashanks7s7@users.noreply.github.com>
2022-07-01 03:34:52 +0545

    delete

--- Final Project BackEnd Api/routes/record.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { postData, getData } = require("../controller/players.js");
-router.post("/post", postData);
-router.get("/get", getData);
-
-module.exports = router;
